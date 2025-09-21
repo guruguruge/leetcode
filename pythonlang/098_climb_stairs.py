8b8e650e9e11771dd7d5beb83a11ac682d1c19e0
Guruge <shionguruge123@gmail.com>
2025-09-21 23:27:43 +0900

    a

---
+++ pythonlang/098_climb_stairs.py
@@ -0,0 +1,19 @@
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         if n == 0 or n == 1:
+#             return 1
+
+#         return self.climbStairs(n - 1) + self.climbStairs(n - 2)
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1:
+            return 1
+        
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
